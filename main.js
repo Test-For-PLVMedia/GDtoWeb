f31ccd2b1a6e6fb3b4d183c9d67bf5a84c52426b
Test-For-PLVMedia <100628385+Test-For-PLVMedia@users.noreply.github.com>
2022-03-07 23:50:12 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -101,9 +101,6 @@ var processFile = function(file, imgs = null) {
     // settings.index : str
     //      "main page" -> index.md
 
-    // Set up array of objects. {section, text}
-    var mdStore = [];
-
     // Fix links
     const linkLUT = {};
     const h1s = doc.getElementsByTagName("h1");
@@ -135,7 +132,7 @@ var processFile = function(file, imgs = null) {
         };
     };
 
-    console.log(linkLUT);
+    // console.log('Link LUT:', linkLUT);
 
     const allLinks = Array.from(doc.getElementsByTagName("a"));
     for (let i = 0; i < allLinks.length; i++) {
@@ -147,8 +144,64 @@ var processFile = function(file, imgs = null) {
             link.setAttribute('href', linkLUT[link.getAttribute('href').replace('#', '')]);
         }
     }
+
+//    // Fix lists
+//    // First, convert all ol to ul
+//    var allOl = doc.getElementsByTagName('ol');
+//    // idc abt efficent algs and tree traversal this works for me
+//    // if we were here for efficency i'd be writing this whole program differently (or not at all)
+//    // don't judge me
+//    while (doc.getElementsByTagName('ol').length > 0) {
+//        for (let i = 0; i < allOl.length; i++) {
+//            const e = allOl[i];
+//            var d = doc.createElement('ul');
+//            d.innerHTML = e.innerHTML;
+//            d.classList = e.classList;
+//            e.parentNode.replaceChild(d, e);
+//        };
+//    }
+//
+//    var lists = getAllLists(doc);
+//    var listOfListIds = getListIds(lists);
+//    console.log(listOfListIds);
+//    for (let i = 0; i < listOfListIds.length; i++) {
+//        // get a full list
+//        const list = lists.filter(l => l.id == listOfListIds[i]);
+//
+//        const listHead = list[0];
+//        var lastLevel = 0;
+//        // For a single list, iterate and re-order.
+//        for (let j = 1 /* Skip list head */; j < list.length; j++) {
+//            const ul = list[j];
+//            if (ul.level == 0) { 
+//                appendAllChildren(listHead.element, ul.element.children); // not a fan of side-effect functions but i'm gonna have to make do and die mad, im too deep. we love the sunk cost fallacy :D
+//                ul.element.remove();
+//            } else if (lastLevel) {
+//                findAndAppendToAbsoluteLastChildYesThisFunctionNameIsVerboseAndHonestlyImHereForIt(listHead.element, ul.element);
+//            }
+//            // im keeping this test code here as a memorial to my mental process figuring out what i was gonna do.
+//            // if (ul.level == 1) {
+//            //     var lastHeadChild = listHead.element.children[listHead.element.children.length - 1];
+//            //     lastHeadChild.append(ul.element);
+//            // }
+//            // if (ul.level == 2) {
+//            //     var lastHeadChild = listHead.element.children[listHead.element.children.length - 1];
+//            //     var lastChildChild = lastHeadChild.children[lastHeadChild.children.length - 1];
+//            //     var lastChildChildChild = lastChildChild.children[lastChildChild.children.length - 1];
+//            //     lastChildChildChild.append(ul.element);
+//            // }
+//            //lastHeadChild.appendChild()
+//            lastLevel = ul.level;
+//        };
+//
+//    };
+//    console.warn(lists);
+
+    console.log('Document after all changes:', doc);
     
-    // Find all sections
+    // Set up array of objects. {section, text}
+    var mdStore = [];
+    // Find all h1's and build the actual markdown pages
     var done = false;
     var startElement = null;
     while (!done) {
@@ -168,13 +221,71 @@ var processFile = function(file, imgs = null) {
         };
     };
 
-    // TODO: Build Index Page
+    // Build Index Page
     var indexPage = buildIndexPage(doc, settings.index, linkLUT, settings.exclude);
     mdStore.push({section: "index", content: indexPage});
     
-    console.log("Got these markdown files:", mdStore);
+    console.log("Made these markdown files:", mdStore);
 
     writeOutToZip(mdStore, imgs);
+}
+
+var appendToLevel = function (head, listObj) {
+    // ODD NUMBERS.
+    // SEE NOTEBOOK.
+}
+
+// similar to "im keeping this test code here as a memorial to my mental process figuring out what i was gonna do." because i was truly going insane (am currently as of writing this)
+var findAndAppendToAbsoluteLastChildYesThisFunctionNameIsVerboseAndHonestlyImHereForIt = function (head, thingToBeAppendedImAlreadyVerboseEnoughAlreadyIMightAsWellCommitToIt) {
+    var done = false;
+    var currentElement = head;
+    while (!done) {
+        var lastItem = currentElement.children[currentElement.children.length - 1];
+        if (lastItem.tagName.toLowerCase() == 'span') {
+            // you've reached the bottom. congratulhatchiuns.
+            currentElement.append(thingToBeAppendedImAlreadyVerboseEnoughAlreadyIMightAsWellCommitToIt);
+            return;
+        } else {
+            currentElement = lastItem;
+        }
+    }
+}
+
+var appendAllChildren = function (to, from) {
+    for (let i = 0; i < from.length; i++) {
+        const child = from[i];
+        to.append(child);
+    };
+};
+
+var getListIds = function(lists) {
+    var ids = [];
+    for (let i = 0; i < lists.length; i++) {
+        const l = lists[i];
+        if (!ids.includes(l.id)) {
+            ids.push(l.id)
+        }
+    }
+    return ids;
+}
+
+var getAllLists = function(doc) {
+    var lists = []; // [] of HTMLCollection
+
+    // All lists should be ul's by now.
+    var allUl = doc.getElementsByTagName('ul');
+    for (let i = 0; i < allUl.length; i++) {
+        const ul = allUl[i];
+        var id = ul.classList.value.match(/\blst-kix_\w+/)[0]; // matches lst-kix_[IDSTUFF] but NOT the last little bit with -[number], bc that changes
+        if (id) {
+            // get the -[number] level (nested level)
+            var level = ul.classList.value.match(/\blst-kix_[\w-]+/)[0].split('-')[2];
+            var doesContainStart = ul.classList.value.includes('start');
+            lists.push({element: ul, id: id, level: level, isStart: doesContainStart, order: i}); // order might not be needed...
+        }
+    }
+
+    return lists;
 }
 
 var buildIndexPage = function (doc, index, linkLUT, exclude) {
@@ -188,13 +299,18 @@ var buildIndexPage = function (doc, index, linkLUT, exclude) {
     // Intro (TODO: Make decent)
     const topTitle = document.createElement("h1");
     topTitle.innerText = "The PLV Media Manual";
-    const topSubtitle = document.createElement("h1");
-    topSubtitle.innerText = "Ethan Harvey";
+    const topSubtitle = document.createElement("h2");
+    topSubtitle.innerText = "Ethan Harvey, et al.";
     const para = document.createElement("p");
-    para.innerText = "Welcome to the PLV Media Manual, a guide to all things media!"
+    para.innerText = "Welcome to the PLV Media Manual, a guide to all things Media Academy! Click one of the links below to be taken to a section."
+    const time = document.createElement("p");
+    const italics = document.createElement("i");
+    italics.innerText = "Site Last Updated: " + Date().toLocaleString();
+    time.appendChild(italics);
     indexDocument.body.appendChild(topTitle);
     indexDocument.body.appendChild(topSubtitle);
     indexDocument.body.appendChild(para);
+    indexDocument.body.appendChild(time);
 
     // [Index Page] - if exists.
     var section = null;
@@ -341,10 +457,10 @@ var writeOutToZip = async function(markdown, imgs = null){
     // get the zip file as a Blob
     const blob = blobWriter.getData();
     
-    saveAs(blob, "markdown-out.zip");
+    // saveAs(blob, "markdown-out.zip");
     updateMessage('Finished! Downloading zip file...');
 }
 
 var updateMessage = function(msg){
-    $('#output')[0].innerText = msg;
+    $('#output')[0].innerHTML += '<br>' + msg;
 };
